a391566782c19b73d79a401653b2cf1f10110fad
es-e2b <ensu1997@naver.com>
2025-05-10 02:09:07 +0900

    [개발] DontDestroyOnLoad 유틸리티 클래스 구현

---
+++ Assets/Scripts/Utility/DontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+namespace Assets.Scripts.Utility
+{
+    using UnityEngine;
+    
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
