eb1e9fc5adcbb5b93d39348d508dc6bab4e3cab2
es-e2b <ensu1997@naver.com>
2025-05-18 04:26:18 +0900

    [리팩토링] 아이템 상호작용 구현

--- Assets/Scripts/Escape/Inventory/ItemObject.cs
+++ Assets/Scripts/Escape/Inventory/ItemObject.cs
@@ -1,10 +1,36 @@
 namespace Assets.Scripts.Escape.Inventory
 {
+    using Assets.Scripts.Escape.Zoom;
     using Sirenix.OdinInspector;
+    using UnityEngine;
+    using UnityEngine.EventSystems;
 
     public class ItemObject : SerializedMonoBehaviour
     {
         public ItemData ItemData;
-        
+
+        public void UseItemOnTargetObject(PointerEventData eventData)
+        {
+            if (ItemData == null)
+            {
+                return;
+            }
+
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(eventData.position);
+
+            RaycastHit2D[] hit = Physics2D.RaycastAll(worldPoint, Vector2.zero);
+            foreach (RaycastHit2D hit2d in hit)
+            {
+                if (hit2d.collider.CompareTag("Target"))
+                {
+                    ZoomManager zoomManager = hit2d.collider.GetComponentInParent<ZoomManager>();
+                    if (zoomManager.ObjectData != null && zoomManager.ObjectData.ItemData.Name == ItemData.Name)
+                    {
+                        zoomManager.ObjectData.OnItemUsed.Invoke();
+                    }
+                    break;
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Escape/Zoom/ObjectData.cs
@@ -0,0 +1,13 @@
+namespace Assets.Scripts.Escape.Zoom
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using Sirenix.OdinInspector;
+    using Inventory;
+
+    public class ObjectData : SerializedMonoBehaviour
+    {
+        [field: SerializeField] public UnityEvent OnItemUsed { get; private set; }
+        [field: SerializeField] public ItemData ItemData { get; private set; }
+    }        
+}
