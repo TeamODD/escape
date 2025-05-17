068e81652e550d2506e3086d912ef5b27935f308
es-e2b <ensu1997@naver.com>
2025-05-17 23:12:57 +0900

    [리팩토링] 아이템 획득 구현
    1. 볼펜 구현

--- Assets/Scripts/Escape/Inventory/ItemData.cs
+++ Assets/Scripts/Escape/Inventory/ItemData.cs
@@ -7,12 +7,29 @@ namespace Assets.Scripts.Escape.Inventory
     public class ItemData : SerializedMonoBehaviour
     {
         [field: SerializeField] public string Name { get; private set; }
-        [field: SerializeField] public bool IsAcquired { get; set; }
+        [field: SerializeField] private bool _isAcquired;
+        public bool IsAcquired
+        {
+            get => _isAcquired;
+            set
+            {
+                _isAcquired = value;
+                AcquiredMappingItemData();
+            }
+        }
         [field: SerializeField] public DialogueData Description { get; private set; }
         [field: SerializeField] public Sprite Sprite { get; private set; }
+        [field: SerializeField] public ItemData MappingItemData { get; private set; }
         public void Start()
         {
             InventoryManager.Instance.ItemDictionary[Name] = this;
+        }
+        private void AcquiredMappingItemData()
+        {
+            if (!MappingItemData._isAcquired)
+            {
+                MappingItemData._isAcquired = true;
+            }
         }
     }        
 }
