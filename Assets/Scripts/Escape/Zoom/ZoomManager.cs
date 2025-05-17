acbef72f5292755f5d270827f3cc7fef3bc6bdde
es-e2b <ensu1997@naver.com>
2025-05-18 04:26:18 +0900

    [리팩토링] 아이템 상호작용 구현

--- Assets/Scripts/Escape/Zoom/ZoomManager.cs
+++ Assets/Scripts/Escape/Zoom/ZoomManager.cs
@@ -1,5 +1,6 @@
 namespace Assets.Scripts.Escape.Zoom
 {
+    using Assets.Scripts.Escape.Inventory;
     using Sirenix.OdinInspector;
     using UnityEngine;
     using UnityEngine.Events;
@@ -10,13 +11,19 @@ namespace Assets.Scripts.Escape.Zoom
         [field: SerializeField] private Image _zoomImage;
         [field: SerializeField] public UnityEvent OnImageEnabled { get; private set; }
         [field: SerializeField] public UnityEvent OnImageDisabled { get; private set; }
+        public ObjectData ObjectData { get; private set; }
         public void SetZoomImage(Sprite sprite)
         {
             _zoomImage.sprite = sprite;
             OnImageEnabled.Invoke();
         }
+        public void SetObjectData(ObjectData data)
+        {
+            ObjectData = data;
+        }
         public void DeactivateZoomImage()
         {
+            ObjectData = null;
             OnImageDisabled.Invoke();
         }
     }        
---
+++ Assets/Scripts/Utility/UIDragMover.cs
@@ -0,0 +1,45 @@
+namespace Assets.Scripts.Utility
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using UnityEngine.EventSystems;
+
+    public class UIDragMover : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        [field:SerializeField] public UnityEvent<PointerEventData> OnEndDragEvent { get; private set; }
+        [SerializeField] private RectTransform _rectTransform;
+        [SerializeField] private Canvas _canvas;
+        private Vector2 _initialAnchoredPosition;
+        private Vector2 _dragOffset;
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            // 마우스 눌렀을 때 위치 기억
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _canvas.transform as RectTransform,
+                eventData.position,
+                eventData.pressEventCamera,
+                out var localMousePos);
+
+            _dragOffset = _rectTransform.anchoredPosition - localMousePos;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _canvas.transform as RectTransform,
+                eventData.position,
+                eventData.pressEventCamera,
+                out var localPoint))
+            {
+                _rectTransform.anchoredPosition = localPoint + _dragOffset;
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            OnEndDragEvent.Invoke(eventData);
+            _rectTransform.anchoredPosition = _initialAnchoredPosition;
+        }
+    }
+}
