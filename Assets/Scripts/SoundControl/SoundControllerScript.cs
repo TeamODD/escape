bae261510edcaff3dc01ab7cb231517abb87340d
es-e2b <ensu1997@naver.com>
2025-05-23 23:27:34 +0900

    [리팩토링] 사운드 구현

--- Assets/Scripts/SoundControl/SoundControllerScript.cs
+++ Assets/Scripts/SoundControl/SoundControllerScript.cs
@@ -8,24 +8,29 @@ public class SoundControllerScript : MonoBehaviour
     public AudioSource bgmSource; // 메인 오디오 출력되는 audiosource
     public AudioSource effectSource; // 이펙트들이 출력되는 audiosource
     public float volume = 1.0f;
+    
+    private Sequence _bgmSequence;
 
-    public void StartMainBgm(AudioClip inputAudio ) // 메인 bgm 출력 시 
+    public void StartMainBgm(AudioClip inputAudio) // 메인 bgm 출력 시 
     {
-        float volume = bgmSource.volume;
-        Sequence sequence = DOTween.Sequence();
-        if(bgmSource.isPlaying)
+        if (_bgmSequence != null && _bgmSequence.IsPlaying())
         {
-            sequence.Append(bgmSource.DOFade(0, 1f).SetEase(Ease.InQuad));
-            sequence.AppendCallback(()=>bgmSource.Stop());
+            _bgmSequence.Kill();
         }
-        sequence.AppendCallback(()=>
+        _bgmSequence = DOTween.Sequence();
+        if (bgmSource.isPlaying)
+        {
+            _bgmSequence.Append(bgmSource.DOFade(0, 1f).SetEase(Ease.InQuad));
+            _bgmSequence.AppendCallback(() => bgmSource.Stop());
+        }
+        _bgmSequence.AppendCallback(() =>
         {
             bgmSource.clip = inputAudio;
             bgmSource.volume = 0f;
             bgmSource.Play();
         });
-        sequence.Append(bgmSource.DOFade(volume, 10f).SetEase(Ease.OutQuad));
-        sequence.Play();
+        _bgmSequence.Append(bgmSource.DOFade(volume, 10f).SetEase(Ease.OutQuad));
+        _bgmSequence.Play();
     }
 
     public void StartEffectBgm(AudioClip inputAudio) //이펙트 소리 내야할 때 해당 상호작용을 일으키는 스크립트에서 원하는 소리를 집어넣은후 호출 
@@ -36,8 +41,7 @@ public class SoundControllerScript : MonoBehaviour
 
     public void VolumeSetting(float volume) //설정에서 소리변경시 모든 사운드가 설정되도록
     {
-        bgmSource.volume = volume;
-        effectSource.volume = volume;
+        this.volume = volume;
     }
     
     
